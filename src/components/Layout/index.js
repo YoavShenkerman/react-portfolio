c7e4e79b3294ff20fa838b991b96382afc7b6b71
yoav <shenkerman1@mail.tau.ac.il>
2025-10-14 14:37:29 +0300

    Add Layout features

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,23 @@
 import './index.scss'
 import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">
+          Portfolio p = new Portfolio();
+          <br />
+          <span className="sub-line">p.run();</span>
+        </span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">{'// End of execution'}</span>
+      </div>
+    </div>
   )
 }
 
