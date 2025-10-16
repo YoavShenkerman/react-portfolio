c0dac9806d76323352748a2489760caa9178630d
yoav <shenkerman1@mail.tau.ac.il>
2025-10-16 17:47:15 +0300

    Add Contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,99 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useRef, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
+  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
+  const userToken = process.env.REACT_APP_EMAILJS_USER_TOKEN
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(serviceId, templateId, refForm.current, userToken).then(
+      () => {
+        alert('Message successfully sent!')
+        window.location.reload(false)
+      },
+      () => {
+        alert('Failed to send the message, please try again')
+      }
+    )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', '', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm currently a Computer Science student looking for a student
+            position in backend or fullstack development. I'm eager to gain
+            hands-on experience and contribute to meaningful projects. However,
+            I'm also open to other opportunities where I can learn, grow, and
+            make an impact - so feel free to reach out using the form below.
+          </p>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/yoav-shenkerman"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+        </div>
+        <div className="contact-form">
+          <form ref={refForm} onSubmit={sendEmail}>
+            <ul>
+              <li className="half">
+                <input type="text" name="name" placeholder="Name" required />
+              </li>
+              <li className="half">
+                <input type="email" name="email" placeholder="Email" required />
+              </li>
+              <li>
+                <input
+                  placeholder="Subject"
+                  type="text"
+                  name="subject"
+                  required
+                />
+              </li>
+              <li>
+                <textarea
+                  placeholder="Message"
+                  name="message"
+                  required
+                ></textarea>
+              </li>
+              <li>
+                <input type="submit" className="flat-button" value="SEND" />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Contact
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,15 +7,7 @@ const Layout = () => {
     <div className="App">
       <Sidebar />
       <div className="page">
-        <span className="tags top-tags">
-          Portfolio p = new Portfolio();
-          <br />
-          <span className="sub-line">p.run();</span>
-        </span>
-
         <Outlet />
-
-        <span className="tags bottom-tags">{'// End of execution'}</span>
       </div>
     </div>
   )
